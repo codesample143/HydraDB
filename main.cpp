04a55535881b3d8fb429e2c3ace20e21c6029aff
definitelynotkevin <definitelynotkevincs@gmail.com>
2025-06-23 17:13:13 +0900

    WAL successful, transactions will begin and end by <Begin> and <End>

--- main.cpp
+++ main.cpp
@@ -7,9 +7,11 @@ int main(int argc, char* argv[]){
     std::unique_ptr<InputBuffer> input_buffer = std::make_unique<InputBuffer>();
     std::fstream dataFile;
     dataFile.open("log.txt", std::ios::in | std::ios::out | std::ios::app);
+    dataFile << "<Begin>" << "\n";
     while(true){
         input_buffer->print_db();
         input_buffer->read_input(input_buffer.get());
+
         if(input_buffer->get_buffer() == ".exit"){
             break;
         }
@@ -18,13 +20,21 @@ int main(int argc, char* argv[]){
             
         }
 
-        if(tolower(input_buffer->get_buffer()[0]) == 'insert'){
-
-        }
+        if(input_buffer->get_buffer().find("insert") != std::string::npos){ //has issues with being able to query, also idk comparison functions in c++, insertion will happen by only. use .find to get a valid index and ensure its not nothing, then write. 
+            //insert ** into ** 
+            //dataFile.write(input_buffer->get_buffer().c_str(), input_buffer->get_buffer().size()); //instead of writing, store in a buffer and only write at the end?
+            dataFile << input_buffer->get_buffer() << "\n";
+            dataFile.flush();
+        } 
         //Statement statement;
         //switch(prepare_statement(input_buffer, &statement)){
 
         //}
     }
+
+    dataFile << "<End>" << "\n";
+    dataFile.close();
+    //dataFile.write();
     //std::cout << input_buffer;
+
 };
