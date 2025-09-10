3904b175427d8ba6e409a657688966ae19a50cb0
= <definitelynotkevincs@gmail.com>
2025-09-10 15:24:33 -0400

    added changes to readme

--- src/installer.cpp
+++ src/installer.cpp
@@ -3,13 +3,9 @@
 #include <filesystem>
 
 int main(){
-    std::string PATH = "/usr/local/bin/";
-
     #ifndef _WIN32
 
     #elif __APPLE__
-        std::string PATH = "/usr/local/bin/";
-        std::system(PATH);
 
     #else
 
