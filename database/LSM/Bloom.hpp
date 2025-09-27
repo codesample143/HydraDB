6b45ed277af70eeb1e5acb4c38ad9aa9a429c3b7
= <definitelynotkevincs@gmail.com>
2025-09-27 14:51:25 -0400

    working from repo directly trying to understand

--- database/LSM/Bloom.hpp
+++ database/LSM/Bloom.hpp
@@ -10,8 +10,18 @@ using namespace std;
 #include <array>
 #include <vector>
 
-class BloomFilter{
+#include "MurmurHash.hpp"
 
+template<class K>
+class BloomFilter{
+    BloomFilter(uint64_t n, double fp){
+        double denom = 0.480453013918201;
+        double size = -1 * (double) n * (log(fp) / denom);
+        m_bits = vector<bool>((int) size);
+        double ln2 = 0.693147180559945;
+        m_numHashes = (int) ceil( (size / n) * ln2);  // ln(2)
+    };
+    
 };
 
 #endif
